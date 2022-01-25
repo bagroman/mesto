70d2a706a8003ad5b0e837fb36cda91c6bc8a85c
Roman Bagautdinov <rrbagautdinov@domclick.ru>
2022-01-25 22:21:45 +0300

    six cards on page load

--- scripts/script.js
+++ scripts/script.js
@@ -10,6 +10,46 @@ let popupHobby = document.querySelector('#popup-hobby-field');
 
 let formElement = document.querySelector('.popup__form');
 
+let elements = document.querySelector('.elements');
+
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
+function fillElements() {
+    const elementTemplate = document.querySelector('#element-template').content;
+    initialCards.forEach(function(item){
+        const element = elementTemplate.querySelector('.element').cloneNode(true);
+        element.querySelector('.element__photo').src = item.link;
+        element.querySelector('.element__photo').alt = item.name;
+        element.querySelector('.element__object-name').textContent = item.name;
+        elements.append(element);
+    })
+}
+
 function showPopup() {
     popup.classList.add('popup_opened');
     popupName.value = profileName.textContent;
@@ -30,3 +70,4 @@ function formSubmitHandler (evt) {
 editPopupButton.addEventListener('click', showPopup);
 closePopupButton.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
+fillElements();
